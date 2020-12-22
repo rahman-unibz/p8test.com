f998448bf949ed0bf242655efcf5156c255bb14d
Siddiqur Rahman <sr@mts-online.com>
2020-12-23 00:03:37 +0100

    update the header block

--- common/js/index.js
+++ common/js/index.js
@@ -1,8 +1,19 @@
 $(function () {
     const swiper = new Swiper('.swiper-container', {
         autoplay: {
-            delay: 10000,
+            delay: 1000000,
         },
+        cssMode: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        scrollbar: {
+            el: '.swiper-scrollbar',
+            hide: false,
+        },
+        mousewheel: true,
+        keyboard: true,
     });
 });
 
