8126b3ca214a824f052575d7612e153f94f5fdc6
Siddiqur Rahman <sr@mts-online.com>
2020-12-24 14:29:50 +0100

    starting footer

--- common/html/footer.php
+++ common/html/footer.php
@@ -1,3 +1,16 @@
 <div class="footer">
-
+    <p>Tele Haase Steuergeräte Ges.m.b.H. | Vorarlberger Allee 38 (Zufahrt: Slamastraße 37) | 1230 Wien |
+        Österreich </p>
+    <p>Tel.: +43 1 614 74–0 | Fax: +43 1 614 74–100 | info@tele-haase.at </p>
+    <ul>
+        <li>
+            <a href="">Datenschutzklärung</a>
+        </li>
+        <li>
+            <a href="">AGB</a>
+        </li>
+        <li>
+            <a href="">Reklamationsservice</a>
+        </li>
+    </ul>
 </div>
